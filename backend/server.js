24289aab22288d6af7263f45269f5d18a63abd91
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-06-21 01:07:29 +0530

    feat: Initialize server setup in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
 import express from "express";
 import cors from "cors";
+import app from "./app.js";
+import connectDb from "./db/connection.js";
+import 'dotenv/config'
 
-const PORT = 5000;
 
-const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 3000;
+const startServer = async () => {
+    try {
+        await connectDb().then(() => {
+            console.log("connected to database");
+        })
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+startServer();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+
+
+
 
