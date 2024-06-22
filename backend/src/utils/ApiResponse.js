317f245a8955aeded2bd396c55c5695ae9421630
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-06-22 23:46:10 +0530

    feat: Add custom API response middleware

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor(status, data, message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+}
+
+export { ApiResponse };
