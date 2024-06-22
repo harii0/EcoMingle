51a959494050bd402d3944fbefa4685bdf8b674f
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-06-22 23:48:24 +0530

    feat:Add custom API Error middleware
        pick d029f5a refactor: restructure project by moving files to src directory
            new file:   src/utils/ApiError.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import { ApiError } from './utils/apiError';
 
 const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
+//error handler
+app.use((err, req, res, next) => {
+  if (err instanceof ApiError) {
+    res.status(err.status).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
 //routes
 
-app.get('/', (req, res) => {
-  res.send('hello');
-});
+
 
 export default app;
--- backend/src/controllers/User.controller.js
+++ backend/src/controllers/User.controller.js
@@ -1,12 +1,42 @@
+import User from '../models/user.model.js';
+import asyncHandler from '../utils/asyncHandler.js'
+import { ApiError } from '../utils/apiError.js';
 //create user
+
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    if ([name, email, password].some((field) => !field?.trim())) {
+        res.status(400);
+        throw new ApiError(400, 'Please add all fields', ['name', 'email', 'password'],);
+    }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        throw new ApiError(409, 'User already exists');
+    }
+
+    const user = await User.create({ name, email, password });
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role,
+            avatar: user.avatar,
+            token: user.generateAccessToken(),
+        });
+    } else {
+        res.status(400);
+        throw new ApiError(400, 'Invalid user data');
+    }
+
+})
+
+//login user
+const loginUser = asyncHandler(async (req, res) => {
+
 })
 //update user
 const updateUser = asyncHandler(async (req, res) => {
-
-})
-//login user
-const loginUser = asyncHandler(async (req, res) => {
 
 })
 //forgot password
