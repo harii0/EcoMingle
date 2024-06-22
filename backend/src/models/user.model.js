ce546d30323c1efbab8c1472e49467bd974571ac
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-06-22 23:47:49 +0530

    feat:Add JWT  for authentication

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -36,15 +37,27 @@ const userSchema = new mongoose.Schema({
 
 //hash password
 userSchema.pre('save', async function (next) {
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 8);
     next();
 });
 //compare hashed pass
-userSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        id: this._id,
+        role: this.role,
+        email: this.email,
+        username: this.username
+    }, process.env.JWT_ACCESS_TOKEN,
+        { expiresIn: process.env.JWT_ACCESS_EXPIRE });
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({ id: this._id },
+        process.env.JWT_REFRESH_TOKEN,
+        { expiresIn: process.env.JWT_REFRESH_EXPIRE });
 }
 const User = mongoose.model('User', userSchema);
 
