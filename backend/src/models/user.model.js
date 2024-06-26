ca68815b7b6a8e7adb30cbcf5a788a21a73bfbb8
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-06-27 00:52:57 +0530

    feat: Add tokens

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,64 +1,87 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     avatar: {
-        type: String,
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        default: 'user',
-        enum: ['user', 'admin'],
+      type: String,
+      default: 'user',
+      enum: ['user', 'admin'],
     },
     orderHistory: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Order',
-        },
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+      },
     ],
-}, { timestamps: true });
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
 
 //hash password
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 8);
-    next();
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 8);
+  next();
 });
 //compare hashed pass
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-}
+  return await bcrypt.compare(password, this.password);
+};
+
+//generate token
+
 userSchema.methods.generateAccessToken = function () {
-    return jwt.sign({
+  try {
+    return jwt.sign(
+      {
         id: this._id,
         role: this.role,
         email: this.email,
-        username: this.username
-    }, process.env.JWT_ACCESS_TOKEN,
-        { expiresIn: process.env.JWT_ACCESS_EXPIRE });
-}
+        username: this.username,
+      },
+      process.env.JWT_ACCESS_TOKEN,
+      { expiresIn: process.env.JWT_ACCESS_EXPIRE },
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 userSchema.methods.generateRefreshToken = function () {
-    return jwt.sign({ id: this._id },
-        process.env.JWT_REFRESH_TOKEN,
-        { expiresIn: process.env.JWT_REFRESH_EXPIRE });
-}
+  try {
+    return jwt.sign({ id: this._id }, process.env.JWT_REFRESH_TOKEN, {
+      expiresIn: process.env.JWT_REFRESH_EXPIRE,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 const User = mongoose.model('User', userSchema);
 
 export default User;
