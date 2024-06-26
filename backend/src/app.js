46a6f11d207edd61c67575d229e8b937a44d0e8f
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-06-27 00:56:35 +0530

    feat: Add cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import { ApiError } from './utils/apiError';
-
+import { ApiError } from './utils/ApiError.js';
+import userRoute from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cookieParser());
+//routes
+app.use('/api/user', userRoute);
+
 //error handler
 app.use((err, req, res, next) => {
   if (err instanceof ApiError) {
     res.status(err.status).json({ message: err.message });
   } else {
-    res.status(500).json({ message: 'Internal server error' })
+    res.status(400).json({ message: 'Something went wrong' });
   }
-})
-//routes
-
-
+});
 
 export default app;
