da92e2a2d339c0ccd485f5f301e46a42928cc3bd
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-06-27 14:07:02 +0530

    add refresh cookie

--- backend/src/controllers/User.controller.js
+++ backend/src/controllers/User.controller.js
@@ -3,7 +3,6 @@ import asyncHandler from '../utils/asyncHandler.js';
 import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 //create user
-
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if ([username, email, password].some((field) => !field?.trim())) {
@@ -22,9 +21,15 @@ const registerUser = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: true,
   };
+  const refreshOptions = {
+    httpOnly: true,
+    secure: true,
+    path: '/refresh_token',
+  };
   return res
     .status(201)
     .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, refreshOptions)
     .json(
       new ApiResponse(201, {
         user: {
@@ -60,9 +65,15 @@ const loginUser = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: true,
   };
+  const refreshOptions = {
+    httpOnly: true,
+    secure: true,
+    path: '/refresh_token',
+  };
   return res
     .status(200)
     .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, refreshOptions)
     .json(
       new ApiResponse(
         200,
@@ -100,8 +111,51 @@ const logoutUser = asyncHandler(async (req, res) => {
     .clearCookie('refreshToken')
     .json(new ApiResponse(200, {}, 'Logged out successfully'));
 });
+//refreshToken
+const refreshToken = asyncHandler(async (req, res) => {
+  const userRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+  if (!userRefreshToken) {
+    throw new ApiError(401, 'Please provide refresh token');
+  }
+  const payload = jwt.verify(userRefreshToken, process.env.JWT_REFRESH_TOKEN);
+  if (!payload) {
+    throw new ApiError(401, 'Invalid refresh token');
+  }
+  const user = await User.findById(payload._id);
+  if (!user) {
+    throw new ApiError(404, 'User not found');
+  }
+  if (user?.refreshToken !== userRefreshToken) {
+    throw new ApiError(401, 'Invalid refresh token');
+  }
+  const accessToken = user.generateAccessToken();
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  const refreshOptions = {
+    httpOnly: true,
+    secure: true,
+    path: '/refresh_token',
+  };
+  return res
+    .status(200)
+    .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', user.refreshToken, refreshOptions)
+    .json(new ApiResponse(200, { accessToken }, 'Refreshed successfully'));
+});
 //update user
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+  //id from params
+  // user from body
+});
 //forgot password
 const forgotPassword = asyncHandler(async (req, res) => {});
-export { registerUser, updateUser, loginUser, logoutUser, forgotPassword };
+export {
+  registerUser,
+  updateUser,
+  loginUser,
+  logoutUser,
+  forgotPassword,
+  refreshToken,
+};
