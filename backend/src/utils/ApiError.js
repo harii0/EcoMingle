a9ef5e170742ea635b4a1e852aee70934ca9a631
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-04 12:20:56 +0530

    fix: :bug: Refactor constructor to include super() call

--- backend/src/utils/ApiError.js
+++ backend/src/utils/ApiError.js
@@ -1,16 +1,17 @@
 class ApiError extends Error {
-    constructor(status, message, errors = [], stack = "") {
-        this.status = status;
-        this.data = null;
-        this.message = message;
-        this.errors = errors;
-        this.success = false;
-        if (stack) {
-            this.stack = stack
-        } else {
-            Error.captureStackTrace(this, this.constructor);
-        }
+  constructor(status, message, errors = [], stack = '') {
+    super(message);
+    this.status = status;
+    this.data = null;
+    this.message = message;
+    this.errors = errors;
+    this.success = false;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
 export { ApiError };
