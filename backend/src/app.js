b5d7821d8a1e5dfa794c3b9a1558876ab5ec5536
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-15 19:46:33 +0530

    feat : add new endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { ApiError } from './utils/ApiError.js';
 import userRoute from './routes/user.route.js';
+import productRoute from './routes/product.route.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 //middleware
@@ -12,7 +13,7 @@ app.use(express.static('public'));
 app.use(cookieParser());
 //routes
 app.use('/api/user', userRoute);
-
+app.use('/api/', productRoute);
 //error handler
 app.use((err, req, res, next) => {
   if (err instanceof ApiError) {
