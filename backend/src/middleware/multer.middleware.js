4584f1b702a2a8ab78f37e2fc382bf853fba7788
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-17 12:53:48 +0530

    feat: add multer middleware

---
+++ backend/src/middleware/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+//FIXME : not working
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadPath = path.join(
+      path.resolve(),
+      '../backend/src/public/uploads',
+    );
+    fs.mkdirSync(uploadPath, { recursive: true });
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
