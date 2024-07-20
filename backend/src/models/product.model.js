27336b8950c2ab2b070c6dd22d37bdf3eb7f3093
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-21 02:08:55 +0530

    added additional fields

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,27 +1,36 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const productSchema = new Schema({
+const productSchema = new Schema(
+  {
     productName: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    image: { type: String, required: true },
-    category: { type: String, required: true },
-    countInStock: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    discount: { type: Number, required: true }
-}, { timestamps: true });
+    ProductImage: { type: [String], required: true },
+    category: {
+      type: String,
+      required: true,
+      set: (value) => value.toLowerCase(),
+    },
+    rating: { type: Number, required: true, min: 0, max: 5 },
+    status: { type: String, default: 'draft' },
+  },
+  { timestamps: true },
+);
 
 const Product = mongoose.model('Product', productSchema);
 
 export default Product;
 
-const productItemSchema = new Schema({
+const productItemSchema = new Schema(
+  {
     productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
     SKU: { type: String, unique: true, required: true },
     inventoryCount: { type: Number, required: true },
-    // other fields
-}, { timestamps: true });
+    discount: { type: Number, required: true },
+  },
+  { timestamps: true },
+);
 
 const ProductItem = mongoose.model('ProductItem', productItemSchema);
 
