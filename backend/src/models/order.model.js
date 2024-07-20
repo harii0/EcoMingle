39a4beca31ae1d3c3a297d589203ca081015d051
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-21 02:08:13 +0530

    feat: added additional fields

--- backend/src/models/order.model.js
+++ backend/src/models/order.model.js
@@ -1,27 +1,43 @@
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
 
-const orderItemSchema = new Schema({
-    item: { type: Schema.Types.ObjectId, ref: 'Product', required: true }
-}, { timestamps: true });
-
-const OrderItem = mongoose.model('OrderItem', orderItemSchema);
-
-const orderSchema = new Schema({
-    orderItems: [{ type: Schema.Types.ObjectId, ref: 'OrderItem', required: true }],
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
     shippingAddress1: { type: String, required: true },
     shippingAddress2: { type: String },
     city: { type: String, required: true },
     zip: { type: String, required: true },
     country: { type: String, required: true },
+
     phone: { type: String, required: true },
-    status: { type: String, default: 'Pending' },
+    status: {
+      type: String,
+      enum: [
+        'pending',
+        'processing',
+        'shipped',
+        'completed',
+        'cancelled',
+        'returned',
+      ],
+      default: 'pending',
+    },
     totalPrice: { type: Number },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    dateOrdered: { type: Date, default: Date.now }
-}, { timestamps: true });
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    dateOrdered: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+);
 
 const Order = mongoose.model('Order', orderSchema);
 
 export default Order;
-export { OrderItem };
