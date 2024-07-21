fba427221c9be1e287071169b1dd0631e788f438
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-22 00:39:43 +0530

    Integrate Mongoose into existing schema

--- backend/src/models/cart.model.js
+++ backend/src/models/cart.model.js
@@ -1,22 +1,22 @@
 import mongoose from 'mongoose';
 
 //cart
-const cartItemSchema = new Schema({
-    productItem: { type: Schema.Types.ObjectId, ref: 'ProductItem' },
-    quantity: {
-        type: Number,
-        required: true,
-        min: 1,
-    }
+const cartItemSchema = new mongoose.Schema({
+  productItem: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
 });
 
-const cartSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
-    items: [cartItemSchema],
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+const cartSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  items: [cartItemSchema],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 const Cart = mongoose.model('Cart', cartSchema);
 
-export default Cart
+export default Cart;
