d3da5d3514e4e0b15382473d7041f16195695ef1
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-22 00:42:51 +0530

    added additional routes

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -8,7 +8,14 @@ import {
   deleteProduct,
   getProductsByCategory,
   addToWishlist,
+  addToCart,
+  removeFromCart,
 } from '../controllers/product.controller.js';
+import {
+  createProductItem,
+  deleteProductItem,
+  updateProductItem,
+} from '../controllers/productItem.controller.js';
 const router = Router();
 
 router.get('/all-products', getAllProducts);
@@ -30,5 +37,12 @@ router.patch(
 router.delete('/delete-product/:id', verifyJwt, isAdmin, deleteProduct);
 router.get('/category', verifyJwt, getProductsByCategory);
 router.put('/wishlist', verifyJwt, addToWishlist);
+router.put('/add-item', verifyJwt, addToCart);
+router.delete('/remove-item', verifyJwt, removeFromCart);
+
+//product items
+router.post('/create-item', verifyJwt, isAdmin, createProductItem); //not admin its vendor
+router.patch('/update-item/:id', verifyJwt, isAdmin, updateProductItem);
+router.delete('/delete-item/:id', verifyJwt, isAdmin, deleteProductItem);
 
 export default router;
