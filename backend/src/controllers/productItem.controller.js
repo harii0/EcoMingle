93ebc328f4b48843b0bbc19febfb1058277372e9
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-07-22 00:35:56 +0530

    add crud functionalities

--- backend/src/controllers/productItem.controller.js
+++ backend/src/controllers/productItem.controller.js
@@ -1,11 +1,11 @@
-import { ProductItem, ProductItem } from '../models/product.model';
-import asyncHandler from '../utils/asyncHandler';
-import { ApiError } from '../utils/ApiError';
-import { ApiResponse } from '../utils/ApiResponse';
+import Product, { ProductItem } from '../models/product.model.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 
-//get product
 const createProductItem = asyncHandler(async (req, res) => {
-  const { SKU, inventoryCount } = req.body;
+  const { SKU, productId } = req.body;
+  const inventoryCount = Number.parseInt(req.body.inventoryCount);
   if (!(productId || SKU || inventoryCount)) {
     throw new ApiError(400, 'please provide all values');
   }
@@ -14,15 +14,47 @@ const createProductItem = asyncHandler(async (req, res) => {
     SKU,
     inventoryCount,
   });
-  await newProductItem.save();
+
+  const product = await Product.findById(productId).populate('productItems');
+  console.log('product', product);
+  product.productItems.push(newProductItem._id);
+  await product.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { product }, 'item created successfully'));
+});
+//update product item
+const updateProductItem = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { SKU, inventoryCount } = req.body;
+  const productItem = await ProductItem.findById(id);
+  if (!productItem) {
+    throw new ApiError(404, 'Product item not found');
+  }
+  productItem.SKU = SKU || productItem.SKU;
+  productItem.inventoryCount = inventoryCount || productItem.inventoryCount;
+  await productItem.save();
+  const updatedProductItem = await ProductItem.findById(id);
   return res
     .status(200)
     .json(
-      new ApiResponse(200, { newProductItem }, 'item created successfully'),
+      new ApiResponse(
+        200,
+        { updatedProductItem },
+        'Product item updated successfully',
+      ),
     );
 });
-//update product item
-
 //delete product item
-
-export { createProductItem };
+const deleteProductItem = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const productItem = await ProductItem.findById(id);
+  if (!productItem) {
+    throw new ApiError(404, 'Product item not found');
+  }
+  await productItem.deleteOne();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, 'Product item deleted successfully'));
+});
+export { createProductItem, updateProductItem, deleteProductItem };
