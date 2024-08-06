21dd8e36066d205d5a5be91d5ecae8880e3e2790
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-07 02:47:29 +0530

    feat : add additional routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import { ApiError } from './utils/ApiError.js';
 import userRoute from './routes/user.route.js';
-import productRoute from './routes/product.route.js';
 import vendorRoute from './routes/vendor.route.js';
+import adminRoute from './routes/admin.route.js';
+import orderRoute from './routes/order.route.js';
+// import handleStripeWebhook from './controllers/webhook.controller.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 //middleware
@@ -13,9 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 //routes
+app.use('/api/admin', adminRoute);
 app.use('/api/user', userRoute);
-app.use('/api/product', productRoute);
 app.use('/api/vendor', vendorRoute);
+app.use('/api/order', orderRoute);
+
+// app.use('/webhook', handleStripeWebhook);
+
 //error handler
 app.use((err, req, res, next) => {
   if (err instanceof ApiError) {
