c57cfd0212dff4974c960843318b52c249eda37a
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-09 21:45:07 +0530

    Refactor file structure: moved model and route"

---
+++ backend/src/features/user/routes/user.route.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshToken,
+  updateUser,
+  forgotPassword,
+  resetPassword,
+  getUserProfile,
+  getUserWhislist,
+  rateProduct,
+} from '../controllers/user.controller.js';
+import { verifyJwt } from '../../../middleware/auth.middleware.js';
+import { verifyResetToken } from '../../../middleware/auth.middleware.js';
+import { upload } from '../../../middleware/multer.middleware.js';
+import {
+  getProductsByCategory,
+  addToWishlist,
+  addToCart,
+  removeFromCart,
+} from '../../product/controllers/product.controller.js';
+const router = Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', verifyResetToken, resetPassword);
+//protect routes
+router.post('/refresh_token', refreshToken);
+router.patch('/update', verifyJwt, upload.single('avatar'), updateUser);
+router.post('/logout', verifyJwt, logoutUser);
+router.get('/profile', verifyJwt, getUserProfile);
+router.get('/wishlist', verifyJwt, getUserWhislist);
+router.post('/ratings/:prodId', verifyJwt, rateProduct);
+
+//products
+router.get('/category', verifyJwt, getProductsByCategory);
+router.put('/wishlist', verifyJwt, addToWishlist);
+router.get('/cart', verifyJwt);
+router.put('/cart', verifyJwt, addToCart);
+router.delete('/cart', verifyJwt, removeFromCart);
+export default router;
