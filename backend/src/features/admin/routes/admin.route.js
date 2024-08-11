9d735613d1b777510aba8dc2e4c17640f8f05a5d
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-11 22:13:13 +0530

    feat : add admin route

---
+++ backend/src/features/admin/routes/admin.route.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import { verifyJwt, isAdmin } from '../../../middleware/auth.middleware.js';
+import {
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  getAppliedVendors,
+  approveVendor,
+  rejectVendor,
+  getVendors,
+  getVendorById,
+  updateVendor,
+  deleteVendor,
+  getProducts,
+  getProductById,
+  deleteProduct,
+} from '../controllers/admin.controller.js';
+const router = Router();
+
+router.get('/users', verifyJwt, isAdmin, getAllUsers);
+router.get('/users/:userId', verifyJwt, isAdmin, getUser);
+router.put('/users/:userId', verifyJwt, isAdmin, updateUser);
+router.delete('/users/:userId', verifyJwt, isAdmin, deleteUser);
+
+// 2. **Vendor Management by Admin**
+// - Get applied vendors
+router.get('/vendors/applications', verifyJwt, isAdmin, getAppliedVendors);
+// post approve vendor
+router.post('/vendors/applications/:vId', verifyJwt, isAdmin, approveVendor);
+// post reject vendor
+router.post('/vendors/applications/:vId', verifyJwt, isAdmin, rejectVendor);
+
+router.get('/vendors', verifyJwt, isAdmin, getVendors);
+router.get('/vendors/:vId', verifyJwt, isAdmin, getVendorById);
+router.post('vendors/:vId', verifyJwt, isAdmin, updateVendor);
+router.delete('vendors/:vId', verifyJwt, isAdmin, deleteVendor);
+
+// 3. **Product Management by Admin**
+router.get('/products', verifyJwt, isAdmin, getProducts);
+router.get('/products/:pId', verifyJwt, isAdmin, getProductById);
+router.delete('/products/:pId', verifyJwt, isAdmin, deleteProduct);
+export default router;
