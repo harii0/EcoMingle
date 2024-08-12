497e08fa8f241a1bc287c42a074c437144d942bd
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-12 17:02:19 +0530

    refactor : import paths

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { ApiError } from './utils/ApiError.js';
-import userRoute from './routes/user.route.js';
-import vendorRoute from './routes/vendor.route.js';
-import adminRoute from './routes/admin.route.js';
-import orderRoute from './routes/order.route.js';
-// import handleStripeWebhook from './controllers/webhook.controller.js';
+import userRoute from './features/user/routes/user.route.js';
+import vendorRoute from './features/vendor/routes/vendor.route.js';
+import adminRoute from './features/admin/routes/admin.route.js';
+import orderRoute from './features/order/order.route.js';
+import handleStripeWebhook from './features/webhook/webhook.controller.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 //middleware
@@ -20,14 +20,14 @@ app.use('/api/user', userRoute);
 app.use('/api/vendor', vendorRoute);
 app.use('/api/order', orderRoute);
 
-// app.use('/webhook', handleStripeWebhook);
+app.use('/webhook', handleStripeWebhook);
 
 //error handler
 app.use((err, req, res, next) => {
   if (err instanceof ApiError) {
     res.status(err.status).json({ message: err.message });
   } else {
-    res.status(400).json({ message: 'Something went wrong' });
+    res.status(400).json({ message: err.message });
   }
 });
 
