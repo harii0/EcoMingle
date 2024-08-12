abdfdc9446ad768181b4fd63eb9aa115a8aceeef
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-12 16:29:23 +0530

    add additional routes

--- backend/src/features/order/order.route.js
+++ backend/src/features/order/order.route.js
@@ -1,17 +1,26 @@
 import { Router } from 'express';
-
+import { verifyJwt } from '../../middleware/auth.middleware.js';
 import {
   createOrder,
+  cancelOrder,
   getOrderById,
   getUserOrders,
   processPayment,
+  confirmPayment,
+  // placeOrder,
+  // getOrderHistory,
+  // getOrderItems,
+  // getOrderStatuses,
+  // updateOrderStatus,
 } from './controllers/order.controller.js';
 
 const router = Router();
 
-router.post('/', createOrder);
+router.get('/my_orders', verifyJwt, getUserOrders);
+router.post('/', verifyJwt, createOrder);
+router.delete('/:orderId', cancelOrder);
 router.get('/:orderId', getOrderById);
-router.get('/user/:userId', getUserOrders);
-router.post('/process-payment', processPayment);
+router.post('/:orderId/process-payment', processPayment);
+router.post('/confirm-payment', verifyJwt, confirmPayment);
 
 export default router;
