84fe996a09afac31cfad07aa68884ab1d8cca4b7
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-14 14:30:18 +0530

    feat: add theme.js for theme customization

---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2B9350',
+    },
+    secondary: {
+      main: '#F3F3F3',
+    },
+    text: {
+      primary: '#3c3c3c',
+      secondary: '#5C5F6A',
+    },
+  },
+});
+
+export default theme;
