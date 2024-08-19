3c35520a67660f1041410fba93623a08159af27e
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-19 20:26:15 +0530

    feat: add button component

--- frontend/src/components/button/Button.jsx
+++ frontend/src/components/button/Button.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/prop-types */
 import { Button } from '@mui/material';
 
-const PrimaryButton = ({ children, props, variant = 'outlined', onClick }) => {
+const PrimaryButton = ({
+  children,
+  props,
+  variant = 'outlined',
+  fontWeight,
+  fontSize,
+  onClick,
+  hovercolor,
+  width,
+}) => {
   return (
     <Button
       onClick={onClick}
       sx={{
+        fontSize: fontSize,
         borderRadius: '50px',
         textTransform: 'none',
+        fontWeight: fontWeight,
+        fontStyle: 'normal',
         color: 'text.primary',
         borderColor: 'text.primary',
         display: 'flex',
+        width: width,
         gap: 1,
+        padding: '8px 14px',
+        '&:hover': {
+          backgroundColor: hovercolor,
+        },
       }}
       variant={variant}
       disableRipple
