eee59714a6b0eca338f95f43f34fdc3542565c52
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-23 21:09:22 +0530

    feat : add auth api

--- frontend/src/features/auth/api.js
+++ frontend/src/features/auth/api.js
@@ -3,3 +3,20 @@ import api from '../../utils/api.js';
 export const login = async (data) => {
   return await api.post('/user/login', data);
 };
+
+export const logout = async () => {
+  return await api.post('/user/logout');
+};
+
+export const register = async (data) => {
+  return await api.post('/user/register', data);
+};
+
+export const forgetPassword = async (data) => {
+  console.log('data', data);
+  return await api.post('/user/forgot-password', data);
+};
+
+export const resetPassword = async (data, token) => {
+  return await api.post(`/user/reset-password?token=${token}`, data);
+};
