3b5d09b1cfbe0c1ed4d9fc82b33da7c162cf08e8
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-23 21:15:45 +0530

    add Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 import App from './App.jsx';
 import './index.css';
-import { ThemeProvider } from '@mui/material';
 import theme from './theme/theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  </Provider>,
 );
