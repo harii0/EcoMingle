e42640d18f5ccdc42bf08b3aafeb491e7d652bda
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-08-26 16:43:12 +0530

    add axios interceptor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
+import './utils/interceptors.js';
 import store from './redux/store.js';
 import App from './App.jsx';
 import './index.css';
