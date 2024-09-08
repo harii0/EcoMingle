1c28239963acc5d1945b23599079d5fde594ebf2
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-09-08 17:45:46 +0530

    fix: removed IconWrapper

--- frontend/src/components/Navbar/navbar.style.js
+++ frontend/src/components/Navbar/navbar.style.js
@@ -2,30 +2,14 @@ import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 
 export const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
   borderRadius: theme.shape.borderRadius,
   border: '1px solid #E0E0E0',
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
-  // marginRight: theme.spacing(2),
-  marginLeft: 0,
-  //   width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-export const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
 }));
 
 export const StyledInputBase = styled(InputBase)(({ theme }) => ({
@@ -34,7 +18,7 @@ export const StyledInputBase = styled(InputBase)(({ theme }) => ({
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     fontSize: '12px',
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: `calc(1em + ${theme.spacing(1)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
