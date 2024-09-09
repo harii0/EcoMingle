1d45025c79639fc4f66966d1bd7192b8069d40c6
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-09-09 17:39:58 +0530

    add BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/appRoutes';
 import Layout from './layout/layout';
 function App() {
   return (
-    <Layout>
-      <AppRoutes />
-    </Layout>
+    <Router>
+      <Layout>
+        <AppRoutes />
+      </Layout>
+    </Router>
   );
 }
 
