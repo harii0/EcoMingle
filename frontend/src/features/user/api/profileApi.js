6a966d0c100c253e106af4193524b1e377b150a9
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-10-20 13:02:35 +0530

    add getOrder api

--- frontend/src/features/user/api/profileApi.js
+++ frontend/src/features/user/api/profileApi.js
@@ -3,3 +3,7 @@ import api from '../../../utils/api.js';
 export const getProfile = async () => {
   return await api.get('/user/profile');
 };
+
+export const getOrders = async () => {
+  return await api.get('/order/my_orders');
+};
