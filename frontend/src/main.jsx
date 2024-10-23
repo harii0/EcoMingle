0d8d5418f05b4e46d0974e18ccf9e1bfde0a5dec
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-10-23 23:53:47 +0530

    add stripe

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import './utils/interceptors.js';
+
 import store from './redux/store.js';
 import App from './App.jsx';
 import './index.css';
 import theme from './theme/theme.js';
 
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+const options = {};
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>
-  </Provider>,
+  <Elements stripe={stripePromise} options={options}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </React.StrictMode>
+    </Provider>
+  </Elements>,
 );
