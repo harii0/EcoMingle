fa3f099fea88ad28522dce10b6a86a568f27eb33
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-10-27 23:55:24 +0530

    add role based auth

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,16 +1,36 @@
 /* eslint-disable react/prop-types */
-
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ element: Component, ...rest }) => {
-  const { isAuthenticated } = useSelector((state) => state.auth);
+const ProtectedRoute = ({ element: Component, requiredRole, ...rest }) => {
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+  console.log(user);
+
   const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated && requiredRole && user.role == requiredRole) {
+      // Redirect based on user role
+      if (user.role === 'vendor') {
+        navigate('/vendor/dashboard');
+      } else if (user.role === 'admin') {
+        navigate('/admin/dashboard');
+      } else {
+        navigate('/dashboard'); // Generic user dashboard
+      }
+    }
+  }, [isAuthenticated, user, navigate, requiredRole]);
 
   if (!isAuthenticated) {
     // Redirect to the login page if the user is not authenticated
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
+  if (requiredRole && user?.data.user.role != requiredRole) {
+    return <Navigate to="/login" replace />;
+  }
+
   return <Component {...rest} />;
 };
 
