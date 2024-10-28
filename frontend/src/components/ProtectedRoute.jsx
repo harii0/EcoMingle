31000d706da6ab660bfb71b479a8252e5b417601
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-10-28 20:32:36 +0530

    refactor Protected route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,36 +1,27 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({ element: Component, requiredRole, ...rest }) => {
+const ProtectedRoute = ({ element: Component, requiredRoles, ...rest }) => {
   const { isAuthenticated, user } = useSelector((state) => state.auth);
-  console.log(user);
-
   const location = useLocation();
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isAuthenticated && requiredRole && user.role == requiredRole) {
-      // Redirect based on user role
-      if (user.role === 'vendor') {
-        navigate('/vendor/dashboard');
-      } else if (user.role === 'admin') {
-        navigate('/admin/dashboard');
-      } else {
-        navigate('/dashboard'); // Generic user dashboard
-      }
-    }
-  }, [isAuthenticated, user, navigate, requiredRole]);
-
+  // Check if user is authenticated
   if (!isAuthenticated) {
-    // Redirect to the login page if the user is not authenticated
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
-  if (requiredRole && user?.data.user.role != requiredRole) {
-    return <Navigate to="/login" replace />;
+
+  // Get user role from state
+  const role = user?.data?.user?.role;
+
+  // If role does not match requiredRole, redirect based on role
+  if (requiredRoles && !requiredRoles.includes(role)) {
+    if (role === 'admin') return <Navigate to="/admin" replace />;
+    if (role === 'vendor') return <Navigate to="/vendor" replace />;
+    return <Navigate to="/" replace />; // Generic user dashboard
   }
 
+  // If all checks pass, render the component
   return <Component {...rest} />;
 };
 
