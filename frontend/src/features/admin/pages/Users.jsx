e67aa3bc0b03863484a982cb0c017cb7c12d523c
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-10-31 23:44:00 +0530

    feat(admin): add get all users

---
+++ frontend/src/features/admin/pages/Users.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import { getAllUsers } from '../api/api';
+import { Paper } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    try {
+      const response = await getAllUsers();
+      console.log(response);
+      setUsers(response.data.data.users);
+      return;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user, index) => (
+              <TableRow
+                key={index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {user._id}
+                </TableCell>
+
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Users;
