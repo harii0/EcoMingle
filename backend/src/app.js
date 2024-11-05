fa749c9e5e973f4b28231588f9dda7bf47db1c69
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-11-05 22:45:59 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ import 'dotenv/config';
 const app = express();
 //middleware
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 };
 app.use(cors(corsOptions));
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import productReducer from '../features/product/productSlice';
 import cartReducer from '../features/user/cartSlice';
-import paymentReducer from '../features/user/paymentSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer.reducer,
     product: productReducer.reducer,
     cart: cartReducer.reducer,
-    payment: paymentReducer,
   },
 });
 
