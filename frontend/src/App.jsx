c7e4148d11ad195fcc2edd4e11d0f1f8657d9768
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-11-05 22:30:47 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/appRoutes';
 import Layout from './layout/layout';
-
 function App() {
   return (
     <Router>
