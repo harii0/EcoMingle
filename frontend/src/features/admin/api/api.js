633133834f5023ebd4484a47fed57cd6a95b05d4
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-11-08 23:24:13 +0530

    add additional api's

--- frontend/src/features/admin/api/api.js
+++ frontend/src/features/admin/api/api.js
@@ -3,3 +3,15 @@ import api from '../../../utils/api.js';
 export const getAllUsers = async () => {
   return await api.get('/admin/users');
 };
+
+export const getUserById = async (id) => {
+  return await api.get(`/admin/users/${id}`);
+};
+
+export const getAllProducts = async () => {
+  return await api.get('/admin/products');
+};
+
+export const getProductById = async (id) => {
+  return await api.get(`/admin/products/${id}`);
+};
