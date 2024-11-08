dfc0da436af1facdc291fbb6bb33dc6fe11e5d41
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-11-08 23:31:12 +0530

    add adminslice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import productReducer from '../features/product/productSlice';
 import cartReducer from '../features/user/cartSlice';
+import adminReducer from '../features/admin/adminSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer.reducer,
     product: productReducer.reducer,
     cart: cartReducer.reducer,
+    admin: adminReducer.reducer,
   },
 });
 
