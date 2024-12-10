125aae2a680ac90edb4993abed54b0ca755ea027
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-12-10 20:37:17 +0530

    add vendor reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,12 +3,14 @@ import authReducer from '../features/auth/authSlice';
 import productReducer from '../features/product/productSlice';
 import cartReducer from '../features/user/cartSlice';
 import adminReducer from '../features/admin/adminSlice';
+import vendorReducer from '../features/vendor/vendorSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer.reducer,
     product: productReducer.reducer,
     cart: cartReducer.reducer,
     admin: adminReducer.reducer,
+    vendor: vendorReducer.reducer,
   },
 });
 
