b18a8fd8991c93dae7deb11c814dba715d01989a
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-12-10 20:34:53 +0530

    add vendor api

---
+++ frontend/src/features/vendor/api/api.js
@@ -0,0 +1,17 @@
+import api from '../../../utils/api';
+
+export const vendorRegister = async (data) => {
+  return await api.post(`/vendor/register`, data, {
+    skipInterceptor: true,
+  });
+};
+
+export const vendorLogin = async (data) => {
+  return await api.post('/vendor/login', data, {
+    skipInterceptor: true,
+  });
+};
+
+export const getProfile = async (vId) => {
+  return await api.get(`/vendor/profile/${vId}`);
+};
