8187ec35f4b5aca125ca1fa5dccb70db426234d4
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-12-13 23:36:40 +0530

    add conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/appRoutes';
 import Layout from './layout/layout';
+import { useSelector } from 'react-redux';
+import VendorRoutes from './routes/vendorRoute';
+import VendorLayout from './layout/vendorLayout';
 function App() {
+  const { vendor, isAuth } = useSelector((state) => state.vendor);
+
+  const role = vendor?.data?.role;
+
   return (
     <Router>
-      <Layout>
-        <AppRoutes />
-      </Layout>
+      {role === 'vendor' || isAuth ? (
+        <VendorLayout>
+          <VendorRoutes />
+        </VendorLayout>
+      ) : (
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      )}
     </Router>
   );
 }
