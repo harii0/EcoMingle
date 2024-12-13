3bca964e468d619e510f0730c2a0c925c2fdafbe
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-12-13 23:41:06 +0530

    add products route

--- frontend/src/routes/vendorRoute.jsx
+++ frontend/src/routes/vendorRoute.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import VendorDashboard from '../features/vendor/pages/VendorDashboard';
 import Profile from '../features/vendor/pages/Profile';
+import Products from '../features/vendor/pages/Products';
 
 const VendorRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const VendorRoutes = () => {
       <Route path="*" element={<Navigate to={'/vendor-dashboard'} replace />} />
       <Route path="/vendor-dashboard" element={<VendorDashboard />} />
       <Route path="/vendor/profile" element={<Profile />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 };
